17ef1dfd5224a4cd4a211fea0d8946348faa5f08
arjn-26 <arjunvramachandran@gmail.com>
2024-07-14 15:55:45 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-       SystemUiOverlayStyle(
+      SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         systemNavigationBarColor: litePrimary,
       ),
